73641d1380f28ece6bc62ea1598b6a17d0eccf68
Gabor Nagymajtenyi <gabor.nagymajtenyi@ppt.eu>
2015-10-23 10:43:15 +0200

    TESTING configuration

--- test/test_an_api.rb
+++ test/test_an_api.rb
@@ -17,7 +17,8 @@ class ANApi < APITest
   end
 
   def test_com_post
-    data = JSON.parse(File.read('data/ancom.json'))
+    path = Pathname(File.expand_path(File.dirname(__FILE__)) + '/../data' )
+    data = JSON.parse(File.read(path + 'ancom.json'))
     post '/api/com', data
 #    assert last_response.ok? #201 valamiért nem ok
     assert JSON.parse(last_response.body), '{"result":"[{:object=>"d1", :success=>true}, {:object=>"s1", :success=>true}, {:object=>"d2", :success=>true}]","success":true}'
