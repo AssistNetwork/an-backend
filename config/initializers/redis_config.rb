2f1c96ea14b9b78f61d02abc4e1ccd0c96215580
Gabor Nagymajtenyi <gabor.nagymajtenyi@ppt.eu>
2015-11-24 13:24:17 +0100

    FrontEnd testing

--- config/initializers/redis_config.rb
+++ config/initializers/redis_config.rb
@@ -1,5 +1,5 @@
 AN.configure do |an|
   an.redis   = ENV['REDIS']
   an.redis ||= 'redis://localhost:6379' if AN.development? or AN.test?
-  AN.logger.error "There is no :redis_config provided!  Please specify one in ENV['REDIS']." unless an.redis
+#  AN.logger.error "There is no :redis_config provided!  Please specify one in ENV['REDIS']." unless an.redis
 end
